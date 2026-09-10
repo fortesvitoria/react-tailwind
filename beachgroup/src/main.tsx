706fce7fd8b86bb29e55cd1202ab66f4a0afa07b
Vitoria Fortes <fortesvitoria@gmail.com>
2026-09-10 20:40:25 -0300

    rename home files

--- beachgroup/src/main.tsx
+++ beachgroup/src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.tsx'
-import SecaoTexto from './components/SecaoTexto.tsx'
-import SecaoCards from './components/SecaoCards.tsx'
+import SecaoTexto from './components/HomeSecaoTexto.tsx'
+import SecaoCards from './components/HomeSecaoCards.tsx'
 import Footer from './components/Footer.tsx'
-import SecaoCoach from './components/SecaoCoach.tsx'
-import HomeSecaoCardSlider from './components/SecaoCarouselImagens.tsx'
+import SecaoCoach from './components/HomeSecaoCoach.tsx'
+import HomeSecaoCardSlider from './components/HomeSecaoCarouselImagens.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
